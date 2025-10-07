699055d30070f35790eb2435ca146d404f130942
et4232@partner.kit.edu <ka_et4232@uc3n990.localdomain>
2025-10-07 21:49:16 +0200

    solve training probelm, save not tested

--- queue.sh
+++ queue.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #SBATCH --time=00:03:00
-#SBATCH --partition=cpu_il
-module load anaconda3
+#SBATCH --partition=gpu_a100_il
+#SBATCH --gres=gpu:1
 
+module load  devel/cuda/12.8
 # Activate your conda environment
-source activate py10
+conda activate py10
 
 # --- Good Practice: Define file paths using variables ---
 
 # Use the directory where you submitted the job as the base
 # This ensures your paths are correct regardless of where the job runs
-BASE_DIR="$SLURM_SUBMIT_DIR"
+#BASE_DIR="$SLURM_SUBMIT_DIR"
 
 # Define the final output file on the shared filesystem
-OUTPUT_FILE="$BASE_DIR/checkpoints"
+#OUTPUT_FILE="$BASE_DIR/checkpoints"
 
-python train.py --save_location "$OUTPUT_FILE"
+python train.py 
+#--save_location "$OUTPUT_FILE"
--- train.py
+++ train.py
@@ -43,7 +43,7 @@ flags.DEFINE_boolean('offline_evaluation', True, 'Whether to perform evaluations
 flags.DEFINE_boolean('render', True, 'Whether to log the rendering to wandb.')
 flags.DEFINE_integer('updates_per_step', 2, 'Number of updates per step.')
 flags.DEFINE_integer('width_critic', 1, 'Width of the critic network.')
-flags.DEFINE_string('save_location', './checkpoints', 'path to save checkpoints, need to be absolute if on cluster')
+#flags.DEFINE_string('save_location', './checkpoints', 'path to save checkpoints, need to be absolute if on cluster')
 
 def read_pause(save_dir='./checkpoints'):
     if os.path.exists(f'{save_dir}/pause.txt'):
@@ -109,8 +109,9 @@ def main(_):
         observations = next_observations
         observations, terms, truns = env.reset_where_done(observations, terms, truns)
         return observations
-    
-    save_dir = FLAGS.save_location
+
+    submit_dir = os.environ.get('SLURM_SUBMIT_DIR')
+ #   save_dir = FLAGS.save_location
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     save_path = f'{save_dir}/brc-{FLAGS.env_names}-{FLAGS.seed}'
