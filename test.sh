1bc4c74c978e5e0f774712b8e85bec6f67c989c6
anyRandomHuman <tcx513887573@gmail.com>
2025-10-08 14:22:20 +0200

    check if pause works

--- test.sh
+++ test.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-#SBATCH --time=00:01:00
-#SBATCH --partition=cpu_il
-conda init
+#SBATCH --time=12:00:00
+#SBATCH --partition=gpu_h100_il
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=urskl@student.kit.edu
+#SBATCH --mail-type=FAIL,END,START
+#SBATCH --signal=B:USR1@3600
+
+module load  devel/cuda/12.8
+# Activate your conda environment
+eval "$(conda shell.bash hook)"
 conda activate py10
-python test.py
+export MUJOCO_GL=egl
 
-# Run the Python script
+# --- Good Practice: Define file paths using variables ---
 
-# Deactivate the virtual environment
+# Use the directory where you submitted the job as the base
+# This ensures your paths are correct regardless of where the job runs
+#BASE_DIR="$SLURM_SUBMIT_DIR"
+
+# Define the final output file on the shared filesystem
+#OUTPUT_FILE="$BASE_DIR/checkpoints"
+
+python train.py 
+#--save_location "$OUTPUT_FILE"
 conda deactivate
