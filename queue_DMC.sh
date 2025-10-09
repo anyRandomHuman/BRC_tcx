1e8cd5719156660e8d91e013a9df026f6d7e0bc0
anyRandomHuman <tcx513887573@gmail.com>
2025-10-09 15:11:09 +0200

    bash for DMC

---
+++ queue_DMC.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --time=22:00:00
+#SBATCH --partition=gpu_h100_il
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=urskl@student.kit.edu
+#SBATCH --mail-type=FAIL,END,START
+#SBATCH --signal=B:USR1@180
+
+module load  devel/cuda/12.8
+# Activate your conda environment
+eval "$(conda shell.bash hook)"
+conda activate DMC
+export MUJOCO_GL=egl
+
+cleanup_and_save()
+{
+    echo "---"
+    echo "WARNING: Time limit approaching. Triggering final save..."
+    # Tell the python script to save and exit
+    # This could be done by creating a flag file or sending another signal
+    touch "$SLURM_SUBMIT_DIR/pause.flag "
+    # Wait a bit for the python script to react and save
+    sleep 120
+}
+
+trap 'cleanup_and_save' USR1
+
+python train.py --env_names DMC_DOGS
+
+conda deactivate
