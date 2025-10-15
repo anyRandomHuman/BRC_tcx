da89f7e8d091fcd1983b69d8ba91c63401b53eb4
Tao Chenxin <tcx513887573@gmail.com>
2025-10-15 13:19:18 +0200

    set up for test

--- queue.sh
+++ queue.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-#SBATCH --time=22:00:00
-#SBATCH --partition=gpu_h100_il
-#SBATCH --gres=gpu:1
-#SBATCH --mail-user=urskl@student.kit.edu
-#SBATCH --mail-type=FAIL,END,START
-#SBATCH --signal=B:USR1@180
+if [ "$1" == "--test" ]; then
+    echo "Running in test mode with reduced resources."
+    #SBATCH --partition=gpu_a100_short
+    #SBATCH --gres=gpu:1
+    #SBATCH --time=00:10:00
+    TEST_FLAG="--test=True"
+else
+    echo "Running in full mode."
+    #SBATCH --time=22:00:00
+    #SBATCH --partition=gpu_h100_il
+    #SBATCH --gres=gpu:1
+    #SBATCH --mail-user=urskl@student.kit.edu
+    #SBATCH --mail-type=FAIL,END,START
+    #SBATCH --signal=B:USR1@180
+    TEST_FLAG="--test=False"
+fi
+
 
 module load devel/cuda/12.8
 eval "$(conda shell.bash hook)"
@@ -22,6 +33,6 @@ cleanup_and_save()
 trap 'cleanup_and_save' USR1
 
 # Pass all arguments to train.py
-python train.py "$@"
+python train.py "$@" $TEST_FLAG
 
 conda deactivate
