e9fe03e81f112d83da19e81421cb106627226586
Tao Chenxin <tcx513887573@gmail.com>
2025-10-17 11:02:17 +0200

    new tasks

--- train_individual.sh
+++ train_individual.sh
@@ -6,15 +6,17 @@ BASE_SCRIPT="queue.sh"
 
 # Array of flag sets for train.py (not SLURM flags)
 TRAIN_FLAGS=(
-#    "--env_names=h1-walk-v0"
-    # "--env_names=h1-stand-v0"
-    # "--env_names=h1-run-v0"
-#    "--env_names=h1-stair-v0"
-#    "--env_names=h1-crawl-v0"
-    # "--env_names=h1-pole-v0"
-    # "--env_names=h1-slide-v0"
-    "--env_names=h1-hurdle-v0"
-    "--env_names=h1-maze-v0"
+    '--env_names=h1hand-sit_simple-v0',
+    '--env_names=h1hand-sit_hard-v0', 
+    '--env_names=h1hand-balance_simple-v0', 
+    '--env_names=h1hand-balance_hard-v0', 
+    '--env_names=h1hand-reach-v0', 
+    '--env_names=h1hand-spoon-v0', 
+    '--env_names=h1hand-window-v0', 
+    '--env_names=h1hand-insert_small-v0', 
+    '--env_names=h1hand-insert_normal-v0',
+    '--env_names=h1hand-bookshelf_simple-v0', 
+    '--env_names=h1hand-bookshelf_hard-v0', 
 )
 if [ "$1" == "--test" ]; then
     echo "Running in test mode with reduced resources."
