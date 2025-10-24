054b2814555cf6444df5e20d460eb66aa2dd6e8d
anyRandomHuman <tcx513887573@gmail.com>
2025-10-24 11:45:15 +0200

    set evaluate flag

--- jaxrl/agent/update.py
+++ jaxrl/agent/update.py
@@ -80,7 +80,7 @@ def compute_grad_conflict(grads, network_name, remove_ln=True, is_leaf=is_leaf_2
     else:
         conflict_tree = jax.tree.map(_grad_conflict_tree_func, grads)
     fc = flatten_tree(conflict_tree)
-    return {f'{network_name}': fc}
+    return {f'{network_name} gradient conflict': fc}
     
 def compute_per_layer_metrics(tree_func, tree, network_name, remove_ln=True, is_leaf=is_leaf_2d):
     if remove_ln: 
