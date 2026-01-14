ed03fb82ecf3c0bb1adebf538da3d7eb5cbb2023
anyRandomHuman <tcx513887573@gmail.com>
2026-01-14 19:53:04 +0100

    remove unused commands in queue.sh

--- queue.sh
+++ queue.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
-#SBATCH --time=40:00:00  # Uncomment this line
+#SBATCH --time=15:00:00  # Uncomment this line
 #SBATCH --partition=gpu_a100_il
 #SBATCH --gres=gpu:1
-#SBATCH --mail-user=urskl@student.kit.edu
-#SBATCH --mail-type=FAIL,END,START
-#SBATCH --signal=B:USR1@180
 
 module load devel/cuda/12.8
 eval "$(conda shell.bash hook)"
 conda activate py10
 export MUJOCO_GL=egl
-
-cleanup_and_save()
-{
-    echo "---"
-    echo "WARNING: Time limit approaching. Triggering final save..."
-    touch "$SLURM_SUBMIT_DIR/pause.flag"
-    sleep 120
-}
-
-trap 'cleanup_and_save' USR1
 
 # Pass all arguments to train.py
 python train.py "$@"
