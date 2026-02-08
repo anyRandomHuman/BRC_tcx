1e712e022123a395ff6f89b6437c08d3a4824329
anyRandomHuman <tcx513887573@gmail.com>
2026-02-08 12:02:14 +0100

    change evaluate(log conflict rate) to false

--- train.py
+++ train.py
@@ -11,37 +11,34 @@ from jaxrl.normalizer import RewardNormalizer
 from jaxrl.logger import EpisodeRecorder
 from jaxrl.env_names import get_environment_list
 
-
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer('seed', 2, 'Random seed.')
-flags.DEFINE_integer('eval_episodes', 10, 'Number of episodes used for evaluation.')
-flags.DEFINE_integer('eval_interval', 5000, 'Eval interval.')
-flags.DEFINE_integer('max_steps', int(1000000), 'Number of training steps.')
-flags.DEFINE_integer('replay_buffer_size', int(1000000), 'Replay buffer size.')
-flags.DEFINE_integer('start_training', int(5000),'Number of training steps to start training.')
-flags.DEFINE_string('env_names', 'pendulum-spin', 'Environment name.')
+flags.DEFINE_integer('seed', 0, 'Random seed.')
+flags.DEFINE_integer('eval_episodes', 5, 'Number of episodes used for evaluation.')
+flags.DEFINE_integer('eval_interval', 10000, 'Eval interval.')
+flags.DEFINE_integer('batch_size', 1024, 'Mini batch size.')
+flags.DEFINE_integer('max_steps', 1000000, 'Number of training steps.')
+flags.DEFINE_integer('replay_buffer_size', 1000000, 'Replay buffer size.')
+flags.DEFINE_integer('start_training', 5000, 'Number of training steps to start training.')
+flags.DEFINE_string('env_names', 'cheetah-run', 'Environment name.')
 flags.DEFINE_boolean('log_to_wandb', True, 'Whether to log to wandb.')
-flags.DEFINE_boolean('evaluate', True, 'Whether to evaluate')
 flags.DEFINE_boolean('offline_evaluation', True, 'Whether to perform evaluations with temperature=0.')
 flags.DEFINE_boolean('render', False, 'Whether to log the rendering to wandb.')
 flags.DEFINE_integer('updates_per_step', 2, 'Number of updates per step.')
 flags.DEFINE_integer('width_critic', 4096, 'Width of the critic network.')
-flags.DEFINE_integer('time', 144000, 'Width of the critic network.')
 flags.DEFINE_string('save_path', '', 'Environment name.')
+flags.DEFINE_string('evaluate', 'False', 'whether to log conflict rate')
+
 
 def main(_):
-    print(f'task: {FLAGS.env_names}')
     if FLAGS.log_to_wandb:
         import wandb
-        import datetime
-        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         wandb.init(
             config=FLAGS,
-            entity='',
-            project=f'BRC',
+            entity='crusaderx',
+            project='BRC',
             group=f'{FLAGS.env_names}',
-            name=f'{FLAGS.env_names}_{current_time}_{FLAGS.seed}'
+            name=f'{FLAGS.env_names}_{FLAGS.seed}'
         )
         
     env_names = get_environment_list(FLAGS.env_names)
@@ -68,73 +65,49 @@ def main(_):
         **kwargs,
     )
     batch_size = 1024 if agent.multitask else 256
-    
-    replay_buffer = ParallelReplayBuffer(env.observation_space, env.action_space.shape[-1], FLAGS.replay_buffer_size, num_tasks=num_tasks)    
-    reward_normalizer = RewardNormalizer(num_tasks, agent.discount) #change max_steps according to env
-    statistics_recorder = EpisodeRecorder(num_tasks)
-    
-    
 
-    def sample(i, observations=None):          
-        actions = env.action_space.sample() if i < FLAGS.start_training else agent.sample_actions(observations, temperature=1.0) #sample for every env
-        next_observations, rewards, terms, truns, goals = env.step(actions) #state shape (num_envs, obs_dim)
-        reward_normalizer.update(rewards, terms, truns)
-        statistics_recorder.update(rewards, goals, terms, truns)
-        masks = env.generate_masks(terms, truns)
-        replay_buffer.insert(observations, actions, rewards, masks, next_observations)
-        observations = next_observations
-        observations, terms, truns = env.reset_where_done(observations, terms, truns)
-        return observations
+    replay_buffer = ParallelReplayBuffer(env.observation_space, env.action_space.shape[-1], FLAGS.replay_buffer_size,
+                                         num_tasks=num_tasks)
+
+    reward_normalizer = RewardNormalizer(num_tasks, target_entropy=agent.target_entropy, discount=agent.discount)
+
+    statistics_recorder = EpisodeRecorder(num_tasks)
+
+    observations = env.reset()
 
     submit_dir = os.environ.get('SLURM_SUBMIT_DIR') if os.environ.get('SLURM_SUBMIT_DIR') is not None else '.'
     save_space = r'/pfs/work9/workspace/scratch/ka_et4232-restored/checkpoints/BRC'
     save_dir = save_space
- #   save_dir = FLAGS.save_location
+    #   save_dir = FLAGS.save_location
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     if FLAGS.save_path == '':
         FLAGS.save_path = FLAGS.env_names
     save_path = f'{save_dir}/{FLAGS.save_path}/{FLAGS.seed}'
     os.makedirs(save_path, exist_ok=True)
-    
 
-    obs = env.reset()
-    start_iter = 0
-    while os.path.exists(f'{save_path}/actor.txt'):
-        # agent.load(save_path)
-        save_path = f'{save_path}_new'
-    # if os.path.exists(f'{save_path}/buffer_info'):
-    #     replay_buffer.load(save_path)
-    # else:
-    for i in range(FLAGS.start_training):
-        obs = sample(i, obs)
+    for i in range(1, FLAGS.max_steps + 1):
+        actions = env.action_space.sample() if i < FLAGS.start_training else agent.sample_actions(observations,
+                                                                                                  temperature=1.0)
+        next_observations, rewards, terms, truns, goals = env.step(actions)
+        reward_normalizer.update(rewards, terms, truns)
+        statistics_recorder.update(rewards, goals, terms, truns)
+        masks = env.generate_masks(terms, truns)
+        replay_buffer.insert(observations, actions, rewards, masks, next_observations)
+        observations = next_observations
+        observations, terms, truns = env.reset_where_done(observations, terms, truns)
+        if i >= FLAGS.start_training:
+            batches = replay_buffer.sample(batch_size, FLAGS.updates_per_step)
+            batches = reward_normalizer.normalize(batches, agent.get_temperature())
+            _ = agent.update(batches, FLAGS.updates_per_step, i)
+            if i % eval_interval == 0 and i >= FLAGS.start_training:
+                info_dict = statistics_recorder.log(FLAGS, agent, replay_buffer, reward_normalizer, i, eval_env,
+                                                    render=FLAGS.render)
+        if i > (FLAGS.max_steps / 2) and i % (FLAGS.max_steps / 10):
+            agent.save(save_path)
 
-    import  time
-    start_time = time.time()
-    pause_iter = -1
-
-    for i in range(FLAGS.max_steps - FLAGS.start_training - start_iter):
-        run_time = time.time() - start_time
-        if os.path.exists(f'{submit_dir}/{FLAGS.env_names}_pause.flag'):
-            pause_iter = i
-            os.remove(f'{submit_dir}/{FLAGS.env_names}_pause.flag')
-            break
-        if FLAGS.time - run_time < 300:
-            with open(f'{save_path}/pause.txt', 'w') as f:
-                f.write(f'{i}')
-            print('runtime insufficient, quitting')
-            replay_buffer.save(save_path)
-            break
-        obs = sample(i + FLAGS.start_training, obs)
-        batches = replay_buffer.sample(batch_size, FLAGS.updates_per_step)  # sample randomly from all data,not one per task
-        batches = reward_normalizer.normalize(batches)
-        _ = agent.update(batches, FLAGS.updates_per_step, i)
-        if i % eval_interval == 0 and i >= FLAGS.start_training and FLAGS.evaluate:
-            info_dict = statistics_recorder.log(FLAGS, agent, replay_buffer, reward_normalizer, i, eval_env,
-                                                render=FLAGS.render)
     agent.save(save_path)
-    exit()
 
-            
+
 if __name__ == '__main__':
     app.run(main)
